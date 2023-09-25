c3ccbfd602e1ca685ee3e46f5ecdf91980784a07
J3SS3HVA <jesse.leeuwerink@hva.nl>
2023-09-25 12:38:55 +0200

    start b-rain

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+// sphere follows mouse
+document.addEventListener('mousemove', (e) => {
+
+    const mouseFollow = document.getElementById('mouse-follow');
+    const x = e.clientX - 50; //-50 to center div over mouse
+    const y = e.clientY - 50; 
+    console.log(x);
+     
+    mouseFollow.style.top = `${y}px`;
+    mouseFollow.style.left = `${x}px`;
+ })
+ 
